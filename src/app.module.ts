1995358f925c2533bad7c2fe5ec012574f3bad2f
ruritoBlogger <ruriro0125@gmail.com>
2021-12-08 18:56:41 +0900

    chore: add auto migration system

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,23 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { Industry } from './entities/industry.entity'
 import { IndustryModule } from './industry/industry.module'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), IndustryModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nestday7',
+      entities: [Industry],
+      synchronize: true,
+    }),
+    IndustryModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
