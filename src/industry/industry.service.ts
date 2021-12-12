bad9ec9546c44d4b10e337840c2f4d12ff4ceebf
ruritoBlogger <ruriro0125@gmail.com>
2021-12-12 13:54:53 +0900

    feat: refactor sync func

--- src/industry/industry.service.ts
+++ src/industry/industry.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/function'
+import * as O from 'fp-ts/Option'
+import * as T from 'fp-ts/Task'
+import * as TE from 'fp-ts/TaskEither'
 import { Industry } from 'src/entities/industry.entity'
-import { Repository } from 'typeorm'
+import { InsertResult, Repository } from 'typeorm'
 
 @Injectable()
 export class IndustryService {
@@ -10,9 +15,39 @@ export class IndustryService {
     private readonly industryRepository: Repository<Industry>,
   ) {}
 
-  addIndustry(name: string) {
-    const industry = new Industry(name)
-    return this.industryRepository.insert(industry)
+  findIndustryByName(name: string): TE.TaskEither<Error, boolean> {
+    return pipe(
+      TE.tryCatch(
+        () => this.industryRepository.findOne({ where: { name } }),
+        (error) => new Error(`${error}`),
+      ),
+      TE.map((result) => !!result),
+    )
+  }
+
+  addIndustry(name: string): TE.TaskEither<Error, InsertResult> {
+    return pipe(
+      this.findIndustryByName(name),
+      TE.bind('canIndustryCreate', (isIndustryExist) =>
+        pipe(
+          isIndustryExist
+            ? E.left(new Error('industry is already existed.'))
+            : E.right(true),
+          TE.fromEither,
+        ),
+      ),
+      // mapが流れてくる(=前のbindWでE.leftが流れてこない)時点で処理が成功していると判断
+      // FIXME: ゴリ押しコードはつらいのら〜〜〜
+      TE.map(() =>
+        pipe(
+          TE.tryCatch(
+            () => this.industryRepository.insert(new Industry(name)),
+            (error) => new Error(`${error}`),
+          ),
+        ),
+      ),
+      TE.flatten,
+    )
   }
 
   getIndustyList() {
