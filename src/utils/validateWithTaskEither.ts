95a4931b95cb320894b61eaecde2b49761ff26e6
ruritoBlogger <ruriro0125@gmail.com>
2022-01-09 21:44:18 +0900

    feat: bodyに含まれている情報の型推論をサポートするやつを追加

---
+++ src/utils/validateWithTaskEither.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, HttpException } from '@nestjs/common'
+import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/function'
+import * as TE from 'fp-ts/TaskEither'
+import * as t from 'io-ts'
+
+export function validateProps<T>(
+  props: unknown,
+  codec: t.Type<T>,
+): TE.TaskEither<HttpException, T> {
+  return pipe(
+    codec.decode(props),
+    E.map((result) => E.right(result)),
+    E.getOrElseW(() =>
+      E.left(
+        new BadRequestException(
+          `IndustryAveIndedx props: ${props} is invalid.`,
+        ),
+      ),
+    ),
+    TE.fromEither,
+  )
+}
