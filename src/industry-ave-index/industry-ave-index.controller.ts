7c413f3fe4b73c1585eececa000bbb875e2dfe32
ruritoBlogger <rurito.blog@gmail.com>
2022-01-15 15:08:59 +0900

    feat: 平均指標周りの操作の際に, どの業績の平均指標かという点を明確にした

--- src/industry-ave-index/industry-ave-index.controller.ts
+++ src/industry-ave-index/industry-ave-index.controller.ts
@@ -13,46 +13,58 @@ export class IndustryAveIndexController {
   constructor(private readonly service: IndustryAveIndexService) {}
 
   @Get('index')
-  async getIndexList(): Promise<IndustryAveIndex[]> {
+  async getIndexList(
+    @Param() param: { industryID: number },
+  ): Promise<IndustryAveIndex[]> {
     return await this.service
-      .getIndexList()()
+      .getIndexList(param.industryID)()
       .then((result) => returnWithThrowHttpException(result))
   }
 
   @Post('index')
-  async addIndex(@Body() body: { props: Props }): Promise<IndustryAveIndex> {
+  async addIndex(
+    @Body() body: { props: Props },
+    @Param() param: { industryID: number },
+  ): Promise<IndustryAveIndex> {
     return await this.service
-      .addIndex(body.props)()
+      .addIndex(body.props, param.industryID)()
       .then((result) => returnWithThrowHttpException(result))
   }
 
   @Put('index')
   async updateIndex(
     @Body() body: { id: number; props: Props },
+    @Param() param: { industryID: number },
   ): Promise<IndustryAveIndex> {
     return await this.service
-      .updateIndex(body.id, body.props)()
+      .updateIndex(body.id, param.industryID, body.props)()
       .then((result) => returnWithThrowHttpException(result))
   }
 
   @Get('index/current')
-  async getCurrentIndex(): Promise<IndustryAveIndex> {
+  async getCurrentIndex(
+    @Param() param: { industryID: number },
+  ): Promise<IndustryAveIndex> {
     return await this.service
-      .getCurrentIndex()()
+      .getCurrentIndex(param.industryID)()
       .then((result) => returnWithThrowHttpException(result))
   }
 
   @Get('index/:id')
-  async getIndex(@Param() param: { id: number }): Promise<IndustryAveIndex> {
+  async getIndex(
+    @Param() param: { id: number; industryID: number },
+  ): Promise<IndustryAveIndex> {
     return await this.service
-      .getIndex(param.id)()
+      .getIndex(param.id, param.industryID)()
       .then((result) => returnWithThrowHttpException(result))
   }
 
   @Delete('index/:id')
-  async deleteIndex(@Param() param: { id: number }): Promise<number> {
+  async deleteIndex(
+    @Param() param: { id: number; industryID: number },
+  ): Promise<number> {
     return await this.service
-      .deleteIndex(param.id)()
+      .deleteIndex(param.id, param.industryID)()
       .then((result) => returnWithThrowHttpException(result))
   }
 }
