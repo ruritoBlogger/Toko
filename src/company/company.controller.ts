a920fb64bd56427d6647527be456688f59349adf
ruritoBlogger <ruriro0125@gmail.com>
2022-01-19 18:07:53 +0900

    feat: company controllerを実装

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
+
+import { Company } from '../entities'
+import { returnWithThrowHttpException } from '../utils'
+import { CompanyService } from './company.service'
+import type { Props } from './type'
 
 @Controller('company')
-export class CompanyController {}
+export class CompanyController {
+  constructor(private readonly service: CompanyService) {}
+
+  @Get()
+  async getCompanyList(): Promise<Company[]> {
+    return await this.service
+      .getCompanyList()()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Post()
+  async addCompany(@Body() body: { props: Props }): Promise<Company> {
+    return await this.service
+      .addCompany(body.props)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Put()
+  async updateCompany(
+    @Body() body: { props: Props; id: number },
+  ): Promise<Company> {
+    return await this.service
+      .updateCompany(body.id, body.props)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Get(';id')
+  async getCompany(@Param() param: { id: number }): Promise<Company> {
+    return await this.service
+      .getCompany(param.id)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Delete(':id')
+  async deleteCompany(@Param() param: { id: number }): Promise<number> {
+    return await this.service
+      .deleteCompany(param.id)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+}
