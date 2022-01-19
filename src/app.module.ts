58c27297eb29707b3950c9d63399dd9391d2aa90
ruritoBlogger <rurito.blog@gmail.com>
2022-01-19 18:09:08 +0900

    fix: moduleの更新忘れてた

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 
-import { CompanyController } from './company/company.controller'
 import { CompanyModule } from './company/company.module'
-import { CompanyService } from './company/company.service'
-import { Industry, IndustryAveIndex } from './entities'
+import { Company, Industry, IndustryAveIndex } from './entities'
 import { IndustryModule } from './industry/industry.module'
 import { IndustryAveIndexModule } from './industry-ave-index/industry-ave-index.module'
 
@@ -17,14 +15,12 @@ import { IndustryAveIndexModule } from './industry-ave-index/industry-ave-index.
       username: 'root',
       password: 'password',
       database: 'nestday7',
-      entities: [Industry, IndustryAveIndex],
+      entities: [Industry, IndustryAveIndex, Company],
       synchronize: true,
     }),
     IndustryModule,
     IndustryAveIndexModule,
     CompanyModule,
   ],
-  providers: [CompanyService],
-  controllers: [CompanyController],
 })
 export class AppModule {}
