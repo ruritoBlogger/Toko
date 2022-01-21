4ab075a64ff085cd987edc286e2a4c2a53f9f120
ruritoBlogger <ruriro0125@gmail.com>
2022-01-21 10:38:30 +0900

    fix: Industryのtestで抜けてる箇所を追加した

--- src/industry/industry.service.spec.ts
+++ src/industry/industry.service.spec.ts
@@ -10,9 +10,11 @@ import {
   IndustryAveIndex,
 } from '../entities'
 import { IndustryService } from './industry.service'
+import type { Props } from './type'
 
 describe('IndustryService', () => {
   let service: IndustryService
+  const props: Props = { name: 'correct_param' }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -50,7 +52,6 @@ describe('IndustryService', () => {
   })
 
   it('should add industry with correct param', async () => {
-    const props = { name: 'correct_param' }
     const result = await service.addIndustry(props)()
 
     // FIXME: ここで削除したくもないし、magic numberも使いたくもない
@@ -67,14 +68,63 @@ describe('IndustryService', () => {
   })
 
   it('should not add industry with same name param', async () => {
-    const props = { name: 'same_name_param' }
     await service.addIndustry(props)()
     const secondIndustry = await service.addIndustry(props)()
 
     // FIXME: ここで削除したくもないし、magic numberも使いたくもない
-    await service.deleteIndustry(2)()
+    await service.deleteIndustry(1)()
 
     expect(E.isLeft(secondIndustry)).toBe(true)
+  })
+
+  // for updateIndustry
+  it('should not updated with null param', async () => {
+    const industryID = pipe(
+      await service.addIndustry(props)(),
+      E.map((result) => result.id),
+      E.getOrElseW(() => -100),
+    )
+    const result = await service.updateIndustry(null, industryID)()
+
+    await service.deleteIndustry(industryID)()
+
+    expect(E.isLeft(result)).toBe(true)
+  })
+
+  it('should not updated with same name param', async () => {
+    await service.addIndustry(props)()
+    const industryID = pipe(
+      await service.addIndustry({ name: 'another' })(),
+      E.map((result) => result.id),
+      E.getOrElseW(() => -100),
+    )
+    const result = await service.updateIndustry(props, industryID)()
+
+    await service.deleteIndustry(1)()
+    await service.deleteIndustry(industryID)()
+
+    expect(E.isLeft(result)).toBe(true)
+  })
+
+  it('should updated with correct param', async () => {
+    const industryID = pipe(
+      await service.addIndustry(props)(),
+      E.map((result) => result.id),
+      E.getOrElseW(() => -100),
+    )
+    const anotherProps: Props = { name: 'another ' }
+    const result = await service.updateIndustry(anotherProps, industryID)()
+
+    await service.deleteIndustry(industryID)()
+
+    expect(E.isRight(result)).toBe(true)
+    expect(
+      pipe(
+        result,
+        E.map((industry) => industry.name),
+        E.getOrElseW(() => 'this test will fail'),
+      ),
+    ).toBe(anotherProps.name)
   })
 
   // for getIndustryList
@@ -91,13 +141,13 @@ describe('IndustryService', () => {
 
   it('should get correct industry data', async () => {
     const industry = pipe(
-      await service.addIndustry({ name: 'test' })(),
+      await service.addIndustry(props)(),
       E.getOrElseW(() => 'addIndustry failed...'),
     )
     const result = await service.getIndustyList()()
 
     // FIXME: ここで削除したくもないし、magic numberも使いたくもない
-    await service.deleteIndustry(3)()
+    await service.deleteIndustry(1)()
 
     expect(E.isRight(result)).toBe(true)
     expect(
@@ -116,7 +166,7 @@ describe('IndustryService', () => {
 
   it('should delete with correct data', async () => {
     const industryID = pipe(
-      await service.addIndustry({ name: 'test' })(),
+      await service.addIndustry(props)(),
       E.map((result) => result.id),
       E.getOrElseW(() => -100),
     )
@@ -138,7 +188,7 @@ describe('IndustryService', () => {
 
   it('should get correct data', async () => {
     const industryID = pipe(
-      await service.addIndustry({ name: 'test' })(),
+      await service.addIndustry(props)(),
       E.map((result) => result.id),
       E.getOrElseW(() => -100),
     )
