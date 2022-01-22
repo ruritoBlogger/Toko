51667b072e474bc4b215674790e3a9565ced06a8
ruritoBlogger <ruriro0125@gmail.com>
2022-01-23 08:21:15 +0900

    feat: IncomeStatementの中身を実装

--- src/income-statement/income-statement.controller.ts
+++ src/income-statement/income-statement.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 
-@Controller('income-statement')
-export class IncomeStatementController {}
+import { IncomeStatement } from '../entities'
+import { returnWithThrowHttpException } from '../utils'
+import { IncomeStatementService } from './income-statement.service'
+import type { Props } from './type'
+
+@Controller('company/:companyID/finantial/:finantialID')
+export class IncomeStatementController {
+  constructor(private readonly service: IncomeStatementService) {}
+
+  @Get('income')
+  async getStatementList(
+    @Param() param: { finantialID: number },
+  ): Promise<IncomeStatement[]> {
+    return await this.service
+      .getStatementList(param.finantialID)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Post('income')
+  async addStatement(
+    @Body() body: { props: Props },
+    @Param() param: { finantialID: number },
+  ): Promise<IncomeStatement> {
+    return await this.service
+      .addStatement(body.props, param.finantialID)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Put('income')
+  async updateStatement(
+    @Body() body: { id: number; props: Props },
+    @Param() param: { finantialID: number },
+  ): Promise<IncomeStatement> {
+    return await this.service
+      .updateStatement(body.props, body.id, param.finantialID)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Get('income/:id')
+  async getStatement(
+    @Param() param: { id: number; finantialID: number },
+  ): Promise<IncomeStatement> {
+    return await this.service
+      .getStatement(param.id, param.finantialID)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+
+  @Delete('income/:id')
+  async deleteStatement(
+    @Param() param: { id: number; finantialID: number },
+  ): Promise<number> {
+    return await this.service
+      .deleteStatement(param.id, param.finantialID)()
+      .then((result) => returnWithThrowHttpException(result))
+  }
+}
