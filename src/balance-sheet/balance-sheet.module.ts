58230ad0077de07913f62210f8f22a0cb70d15d3
ruritoBlogger <ruriro0125@gmail.com>
2022-02-07 07:15:49 +0900

    fix: いくつかの機能がmappingされてなかったので対応

--- src/balance-sheet/balance-sheet.module.ts
+++ src/balance-sheet/balance-sheet.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
-@Module({})
+import {
+  BalanceSheet,
+  Cashflow,
+  FinantialStatements,
+  IncomeStatement,
+  Index,
+} from '../entities'
+import { BalanceSheetController } from './balance-sheet.controller'
+import { BalanceSheetService } from './balance-sheet.service'
+
+@Module({
+  controllers: [BalanceSheetController],
+  imports: [
+    TypeOrmModule.forFeature([
+      IncomeStatement,
+      FinantialStatements,
+      Cashflow,
+      BalanceSheet,
+      Index,
+    ]),
+  ],
+  providers: [BalanceSheetService],
+})
 export class BalanceSheetModule {}
--- src/index/index.module.ts
+++ src/index/index.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
-@Module({})
+import {
+  BalanceSheet,
+  Cashflow,
+  FinantialStatements,
+  IncomeStatement,
+  Index,
+} from '../entities'
+import { IndexController } from './index.controller'
+import { IndexService } from './index.service'
+
+@Module({
+  controllers: [IndexController],
+  imports: [
+    TypeOrmModule.forFeature([
+      IncomeStatement,
+      FinantialStatements,
+      Cashflow,
+      BalanceSheet,
+      Index,
+    ]),
+  ],
+  providers: [IndexService],
+})
 export class IndexModule {}
