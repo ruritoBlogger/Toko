710a7960cbaacbf97009703f60134018ef8cf270
ruritoBlogger <rurito.blog@gmail.com>
2022-03-20 16:36:39 +0900

    chore: CORSの設定を追加

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  })
+
   await app.listen(3000)
 }
 bootstrap()
